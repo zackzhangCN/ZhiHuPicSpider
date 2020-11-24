32a921b097870399f43246db4e943d692425f551
zackzhangCN <2512704509@qq.com>
2020-11-24 19:44:15 +0800

    改个名

--- src/main/java/cn/zack/ZhiHuPicSpiderApplication.java
+++ src/main/java/cn/zack/ZhiHuPicSpiderApplication.java
@@ -8,14 +8,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import java.awt.*;
 
 @SpringBootApplication
-public class ZhiHuUserInfoApplication {
+public class ZhiHuPicSpiderApplication {
     public static void main(String[] args) {
         /**
          * 初始化spring
          * 设置headless为false, 否则会报java.awt.HeadlessException
          * java.awt.headless是J2SE的一种模式, 用于在缺失显示屏、鼠标或者键盘时的系统配置, springboot默认将这个属性设置为true
          */
-        new SpringApplicationBuilder(ZhiHuUserInfoApplication.class).headless(false).run(args);
+        new SpringApplicationBuilder(ZhiHuPicSpiderApplication.class).headless(false).run(args);
 
         /**
          * 加载UI界面
